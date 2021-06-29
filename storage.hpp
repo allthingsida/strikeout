95a04a50e9f09012a37d4ccd9806bccf4f19054a
Elias Bachaalany <elias.bachaalany@gmail.com>
2021-06-28 18:02:22 -0700

    0.1.2alpha: added support for database relocation
    - fixed a last minute bug in the loading code from the previous release

--- storage.hpp
+++ storage.hpp
@@ -9,36 +9,54 @@ class eanodes_t
     easet_t eas; // Cached copy of inverted if-statement addresses
     netnode node;
     qstring nodename;
+    bool use_relative;
 
 public:
-    eanodes_t(const char *nodename): nodename(nodename) { load(); }
+    eanodes_t(const char *nodename, bool use_relative=true): nodename(nodename), use_relative(use_relative) { load(); }
     const easet_t& nodes() { return eas; }
 
     bool load()
     {
+        eas.clear();
         if (node.create(nodename.c_str())) // create failed -> exists already
             return false;
 
         size_t n;
+        ea_t image_base = 0;
+        if (use_relative)
+        {
+            image_base = get_imagebase();
+            if (image_base == BADADDR)
+                image_base = 0;
+        }
+
         void* blob = node.getblob(NULL, &n, 0, 'I');
         if (blob != nullptr)
         {
             auto pea = (ea_t*)blob;
             for (size_t i = 0, count = n / sizeof(ea_t); i < count; ++i, ++pea)
-                eas.insert(*pea);
+                eas.insert(*pea + image_base);
+
             qfree(blob);
         }
-        eas.clear();
         return true;
     }
 
     void save()
     {
+        ea_t image_base = 0;
+        if (use_relative)
+        {
+            image_base = get_imagebase();
+            if (image_base == BADADDR)
+                image_base = 0;
+        }
+
         eavec_t copy;
         copy.resize(eas.size());
         size_t idx = 0;
         for (auto ea : eas)
-            copy[idx++] = ea;
+            copy[idx++] = ea - image_base;
 
         node.setblob(copy.begin(), copy.size() * sizeof(ea_t), 0, 'I');
     }
