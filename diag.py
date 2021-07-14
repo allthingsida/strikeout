2b90baa4925cfaed9449ec10373b869a87c26a03
Elias Bachaalany <elias.bachaalany@gmail.com>
2021-07-14 10:20:51 -0700

    Update diag.py
    Improving the Python counterpart to help do some diagnostics and other functionality

--- diag.py
+++ diag.py
@@ -1,39 +1,38 @@
 import idaapi
 
+# --------------------------------------------------------------------------------
 class hidestmt_t:
-    def __init__(self, is64=True):
+    def __init__(self, is64=True, use_relative=True):
         self.n = idaapi.netnode("$ hexrays strikeout-plugin")
         self.c = 'Q' if is64 else 'L'
         self.ptr_size = 8 if is64 else 4
+        self.use_relative = use_relative
+
 
     def load(self):
-        return self.parse_blob(self.n.getblob(0, 'I') or [])
+        addresses = []
+        blob = self.n.getblob(0, 'I') or []
+        imgbase = idaapi.get_imagebase() if self.use_relative else 0
+        for i, offs in enumerate(range(0, len(blob), self.ptr_size)):
+            ea = struct.unpack(self.c, blob[offs:offs+self.ptr_size])[0]
+            addresses.append(imgbase + ea)
+        return addresses
 
 
     def kill(self):
         self.n.kill()
 
-
-    def parse_blob(self, blob):
-        addresses = []
-        for i, offs in enumerate(range(0, len(blob), self.ptr_size)):
-            ea = struct.unpack(self.c, blob[offs:offs+self.ptr_size])[0]
-            addresses.append(ea)
-        return addresses
-
-
-    def pack_addresses(self, addresses):
+    
+    def save(self, addresses):
+        imgbase = idaapi.get_imagebase() if self.use_relative else 0
         b = bytearray()
         for addr in addresses:
-            b += struct.pack(self.c, addr)
-        return bytes(b)
-
-
-    def save(self, addresses):
-        blob = self.pack_addresses(addresses)
+            b += struct.pack(self.c, addr - imgbase)
+        blob = bytes(b)
         self.n.setblob(blob, 0, 'I')
 
 
+# --------------------------------------------------------------------------------
 def compare_blobs(b1, b2):
     if len(b1) != len(b2):
         return -1
@@ -45,27 +44,55 @@ def compare_blobs(b1, b2):
     return 0
 
 
-def main():
-    idaapi.msg_clear()
+# --------------------------------------------------------------------------------
+def clean_func_info(func_ea=idaapi.BADADDR):
+    if func_ea == idaapi.BADADDR:
+        func_ea = idaapi.get_screen_ea()
+    f = idaapi.get_func(func_ea)
+    if not f:
+        return (False, 'No function!')
+    else:
+        func_ea = f.start_ea
+
+    addresses = diag.load()
+    print(f'Effective parent function: {f.start_ea:x}..{f.end_ea:x}')
+
+    new_addresses = []
+    for addr in addresses:
+        f = idaapi.get_func(addr)
+        if f and f.start_ea == func_ea:
+            print(f'Omitting: {addr:x}')
+            continue
+        else:
+            # print(f'Skipping: {addr:x}')
+            pass
+
+        new_addresses.append(addr)
+
+
+    print(f"Old={len(addresses)} New={len(new_addresses)}")
+
+    # Save when change occurs
+    if len(addresses) != len(new_addresses):
+        diag.save(new_addresses)
+
+
+# --------------------------------------------------------------------------------
+def dump():
     global diag
     diag = hidestmt_t()
     addresses = diag.load()
 
+    print('Dumping address\n---------------')
     for ea in addresses:
         print(f"{ea:x} ...")
 
-    # #func_ea = idaapi.get_name_ea(idaapi.BADADDR, 'prot_resolve_callback')
-    # func_ea = idaapi.get_name_ea(idaapi.BADADDR, 'prot_3_0')
-    # new_addresses = []
-    # for addr in addresses:
-    #     f = idaapi.get_func(addr)
-    #     if f and f.start_ea == func_ea:
-    #         continue
-    #     new_addresses.append(addr)
-
-    # print(f"old={len(addresses)} new={len(new_addresses)}")
-    # n.save(new_addresses)
-    # #parent_func = idaapi.get_func(func_ea)
+    print(f'Total {len(addresses)}')
 
 
-main()
+# --------------------------------------------------------------------------------
+if __name__=='__main__':
+    idaapi.msg_clear()
+
+    # dump()
+    clean_func_info()
