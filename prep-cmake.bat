4d106d60060c3a3f4fccbf40de8951b0b576ae34
Elias Bachaalany <lallousx86@users.noreply.github.com>
2023-09-27 06:12:42 -0700

    fixed prep-cmake (to select default VS version)

--- prep-cmake.bat
+++ prep-cmake.bat
@@ -6,7 +6,7 @@ setlocal
 if not exist build64 mkdir build64
 pushd build64
 
-cmake -A x64 -G "Visual Studio 17 2022" .. -DEA64=YES
+cmake -A x64 .. -DEA64=YES
 
 if "%2"=="" (set cfg=Release) else (set cfg=%2)
 if "%1"=="build64" cmake --build . --config %cfg%
@@ -16,7 +16,7 @@ popd
 :: EA64=NO
 if not exist build mkdir build
 pushd build
-cmake -A x64 -G "Visual Studio 17 2022" .. -DEA64=NO
+cmake -A x64 .. -DEA64=NO
 if "%2"=="" (set cfg=Release) else (set cfg=%2)
 if "%1"=="build" cmake --build . --config %cfg%
 popd
